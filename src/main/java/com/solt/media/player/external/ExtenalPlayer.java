b91dd1f75f2f4332ae11aa11934ff7311cf2011a
longdt <4091487+longdt@users.noreply.github.com>
2014-06-07 22:42:28 -0700

    handle action user exit external player

--- src/main/java/com/solt/media/player/external/ExtenalPlayer.java
+++ src/main/java/com/solt/media/player/external/ExtenalPlayer.java
@@ -2,8 +2,15 @@ package com.solt.media.player.external;
 
 import java.io.IOException;
 
+import com.solt.libtorrent.TorrentManager;
+
 public class ExtenalPlayer {
-	private PlayerInstance instance;
+	private volatile PlayerInstance instance;
+	private InstanceMonitor monitor;
+	public ExtenalPlayer() {
+		monitor = new InstanceMonitor();
+		monitor.start();
+	}
 	
 	public void open(String url, String... subFile) {
 		instance = PlayerInstanceFactory.newPlayerInstance();
@@ -21,6 +28,36 @@ public class ExtenalPlayer {
 		}
 	}
 	
+	public void stop() {
+		monitor.shutdown();
+	}
+	
+	class InstanceMonitor extends Thread {
+		private volatile boolean done;
+		public InstanceMonitor() {
+			setDaemon(true);
+		}
+		
+		public void shutdown() {
+			done = true;
+			interrupt();
+		}
 
+		@Override
+		public void run() {
+			try {
+				while (!done) {
+					if (instance == null || instance.isTerminated()) {
+						Thread.sleep(1000);
+						continue;
+					}
+					instance.waitForTerminate();
+					//if use quit external player then torrentmanager cancelstream
+					TorrentManager.getInstance().cancelStream();
+				}
+			} catch (InterruptedException e) {
+			}
+		}
+	}
 	
 }
